0377ae9eaccfca928125522cf52f5e40718dbb0f
RaptorCoding <raptorlearningcode@gmail.com>
2024-03-30 12:18:01 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Client, REST, Routes, GatewayIntentBits } =  require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log('Bot is on !');
+    client.channels.cache.get("1223573780908212256")
+    .send("I'm booted up !")
+});
+
+
+
+client.login(process.env.TOKEN);
+
+
