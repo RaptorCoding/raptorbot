6161ca222b5264a26df648dbd8da1a88e8efcbc9
RaptorCoding <raptorlearningcode@gmail.com>
2024-03-30 12:23:53 +0100

    tags me change if you want

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once('ready', () => {
     console.log('Bot is on !');
     client.channels.cache.get("1223573780908212256")
-    .send("I'm booted up !")
+    .send("I'm booted up ! <@713298191159918612> ")
 });
 
 
