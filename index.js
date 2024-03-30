d0cb71ad0fda3f568bf20bcdd641ccf2835cceab
RaptorCoding <raptorlearningcode@gmail.com>
2024-03-30 12:54:38 +0100

    Started command handler

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const { Client, REST, Routes, GatewayIntentBits } =  require('discord.js');
+const fs = require('node:fs')
+const path = require('node:path')
+const { Client, REST, Routes, GatewayIntentBits, Collection } =  require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => {
@@ -7,6 +9,21 @@ client.once('ready', () => {
     client.channels.cache.get("1223573780908212256")
     .send("I'm booted up ! <@713298191159918612> ")
 });
+const commandPath = path.join(__dirname,"commands")
+const folderPath = fs.readdirSync(commandPath)
+client.commands = new Collection();
+for (folder of commandPath) {
+    const commandsfs = fs.readdirSync("./commands").filter(file => file.endsWith('.js'))
+    const commandName = path.join(folderPath, folder);
+    const command = require(filepath)
+    for (file of command) {
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
 
 
 
